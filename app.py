63c1ed603dcf559c0cb1eb10b10700528e388bd9
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-01-27 21:09:31 +0700

    Create initial database setup with table creation
    Added functionality to create a `contacts` table in the database if it doesn't already exist. Integrated `sqlite3` for database handling and prepared the structure for storing contact details. Added a test block to ensure table creation is executed.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import tkinter as tk
 
 from constants import DATABASE_NAME
 
@@ -11,8 +12,25 @@ class Contact:
         self.address = address
         self.notes = notes
 
-def create_database_table()
-    pass
+def create_database_table() :
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS contacts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first_name TEXT NOT NULL,
+                last_name TEXT,
+                phone TEXT,
+                email TEXT,
+                address TEXT,
+                notes TEXT
+            )
+        ''')
+    conn.commit()
+    conn.close()
 
 def add_contact_to_database(contact):
     pass
+
+if __name__ == "__main__":  # Just for testing
+    create_database_table()
