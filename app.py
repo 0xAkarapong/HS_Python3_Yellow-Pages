37bb570b490019cadd47780b9269eed6ea044e7f
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-01-27 21:27:23 +0700

    Refactor: Move Contact class and add_contact_to_database
    Moved the Contact class and add_contact_to_database function from `app.py` to `database_manager.py` for better modularity and separation of concerns. This improves code organization and keeps database-related logic within the appropriate module.

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 import sqlite3
 import tkinter as tk
 from database_manager import *
-from constants import DATABASE_NAME
-
-class Contact:
-    def __init__(self, first_name, last_name, phone, email, address, notes):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone = phone
-        self.email = email
-        self.address = address
-        self.notes = notes
-
-
-def add_contact_to_database(contact):
-    pass
+from constants import *
 
 if __name__ == "__main__":  # Just for testing
     create_database_table()
--- database_manager.py
+++ database_manager.py
@@ -1,6 +1,19 @@
 from constants import *
 import sqlite3
 
+class Contact:
+    def __init__(self, first_name, last_name, phone, email, address, notes):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.notes = notes
+
+
+def add_contact_to_database(contact):
+    pass
+
 def create_database_table() :
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
